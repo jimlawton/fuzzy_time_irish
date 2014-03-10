2ce7bea7a37637b44b168d4a38e0181be7c4e12a
Jim Lawton <jim.lawton@gmail.com>
2014-03-10 13:53:04 +0000

    First pass at v2 implementation.

--- src/fuzzy_time_irish.c
+++ src/fuzzy_time_irish.c
@@ -1,61 +1,60 @@
 #include <pebble.h>
+#include <time.h>
 #include "num2words.h"
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *textLayer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+#define BUFFER_SIZE 86
+static char sdata[BUFFER_SIZE];
+
+static void update_time(struct tm *t) {
+    fuzzy_time_to_words(t->tm_hour, t->tm_min, sdata, BUFFER_SIZE);
+    text_layer_set_text(textLayer, sdata);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+    APP_LOG(APP_LOG_DEBUG, "Tick!");
+    update_time(tick_time);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    Layer *windowLayer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(windowLayer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    textLayer = text_layer_create((GRect) {
+        .origin = { 0, 72 },
+        .size = { bounds.size.w, 20 }
+    });
+    text_layer_set_text(textLayer, "Press a button");
+    text_layer_set_text_alignment(textLayer, GTextAlignmentCenter);
+    layer_add_child(windowLayer, text_layer_get_layer(textLayer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    text_layer_destroy(text_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+    window = window_create();
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    const bool animated = true;
+    window_stack_push(window, animated);
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+    tick_timer_service_unsubscribe();
+    text_layer_destroy(textLayer);
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
-  app_event_loop();
-  deinit();
+    init();
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+    app_event_loop();
+    deinit();
 }
